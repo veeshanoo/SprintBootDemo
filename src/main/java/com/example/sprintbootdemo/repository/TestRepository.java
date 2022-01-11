2cb7870e4e6a4adafc2fbdd68384291ef5b94ef8
cristi <visanu.cristian@gmail.com>
2022-01-11 02:14:26 +0200

    removed test classes

--- src/main/java/com/example/sprintbootdemo/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.sprintbootdemo.repository;
-
-import com.example.sprintbootdemo.model.TestModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<TestModel, Integer> {
-}
